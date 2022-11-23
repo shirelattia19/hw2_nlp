f1b3bb12c8c7ea5fa7371794bb59d47db8c68d95
shirelattia19 <shirel.attia@campus.technion.ac.il>
2022-11-23 21:22:12 +0200

    preprocessed data without glove unkown

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
-# This is a sample Python script.
-
-# Press Maj+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
+import os.path
+from gensim import downloader
+import numpy as np
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    # preprocess data
+    train_path = os.path.join('data', 'train.tagged')
+    list_of_sentences = []
+    list_of_words = []
+    sentence_index = 0
+    with open(train_path) as f:
+        for line in f:
+            if line == "\t\n" or line == "\n":
+                sentence_index += 1
+                continue
+            if line[-1:] == "\n":
+                line = line[:-1]
+            word, tag = line.split('\t')
+            word = word.strip().lower()
+            list_of_words.append((word, tag))
+            if len(list_of_sentences) <= sentence_index:
+                list_of_sentences.append([(word, tag)])
+            else:
+                list_of_sentences[sentence_index].append((word, tag))
+
+
+    # represent data with glove
+    glove_path = 'glove-twitter-200'
+    glove = downloader.load(glove_path)
+    representation = []
+    for word, tag in list_of_words:
+        if word not in glove.key_to_index:
+            print(f"{word} not an existing word in the model")
+            continue
+        vec = glove[word]
+        representation.append(vec)
+    representation = np.asarray(representation)
+    print(representation.shape)
+
+
+
